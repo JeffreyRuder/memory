c1378cbbdfac1cd1feac37dd45da28b024874d45
James Nielson and Jeffrey Ruder <james@megafauna.org>
2016-02-15 12:00:36 -0800

    Change styling

--- js/browser-ui.js
+++ js/browser-ui.js
@@ -2,14 +2,14 @@ var Game = require('./../js/memory.js').Game;
 var Space = require('./../js/memory.js').Space;
 
 $(document).ready(function() {
-  var thisGame = new Game(4, 3);
+  var thisGame = new Game(4, 4);
   thisGame.setupBoard();
 
   //draw grid
   for (var i = 1; i <= thisGame.rows; i++) {
     $(".game-grid").append("<div class='row' id='row" + i + "'></div>" );
     for (var j = 1; j <= thisGame.columns; j++) {
-      $("#row" + i).append("<div class='cell col-sm-3' id='" + i + "_" + j + "'></div>");
+      $("#row" + i).append("<div class='cell col-sm-2' id='" + i + "_" + j + "'></div>");
     }
   }
 
